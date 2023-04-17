a75f83f73695ea043533bc71b39b5fe145884765
antoninodangelo11 <reapercsp@gmail.com>
2023-04-17 17:09:14 +0200

    end dadi exercise

---
+++ gioco_dei_dadi/assets/script.js
@@ -0,0 +1,22 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+const player = Math.round(Math.random () * 6);
+console.log('Player', player);
+
+const computer = Math.round(Math.random () * 6);
+console.log('Computer', computer);
+
+if (player > computer) {
+    console.log('Il player ha vinto');
+}
+
+else if (player < computer) {
+    console.log('Il player ha perso');
+}
+
+else { 
+    (player == computer)
+    console.log('Pareggio');
+}
